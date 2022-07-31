a71fe7129d6d64baff3f993f3cccb16c81db786f
historydev <historyconceptart@gmail.com>
2022-07-31 13:46:12 +0300

    app.module update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { HeaderComponent } from './components/header/header.component';
 import {SocketIoModule, SocketIoConfig, Socket} from 'ngx-socket-io';
 import {LoggerService} from "./services/logger/logger.service";
 
-const config: SocketIoConfig = { url: 'http://localhost:4000', options: {} };
+const config: SocketIoConfig = { url: '/video', options: {} };
 
 @NgModule({
 	declarations: [
@@ -39,6 +39,7 @@ const config: SocketIoConfig = { url: 'http://localhost:4000', options: {} };
 	providers: [],
 	bootstrap: [AppComponent]
 })
+
 export class AppModule {
 
 	constructor(
